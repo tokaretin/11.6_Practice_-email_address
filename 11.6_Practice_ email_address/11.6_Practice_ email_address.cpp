b999be9f9de3f3731c76cb6d9f4ad7882a0b7f87
tokaretin <tokaretin@gmail.com>
2023-04-27 18:08:40 +0200

    New code

--- 11.6_Practice_ email_address/11.6_Practice_ email_address.cpp
+++ 11.6_Practice_ email_address/11.6_Practice_ email_address.cpp
@@ -1,85 +1,135 @@
 ﻿#include <iostream>
-#include <string>
 
-std::string first_part_email(std::string str) {
-    std::string firstPartEmail;
-    for (int i = 0; str[i] != '@'; i++) {
-        if (i == str.length() - 1)break;
-        firstPartEmail += str[i];
-    }
-    return firstPartEmail;
-}
+using namespace std;
 
-bool error_or_not(std::string str) {
-    if (first_part_email(str).length() == str.length()) return false;
-}
-
-bool first_part_is_valid(std::string str) {
-    if (!error_or_not(str)) return false;
-    std::string validCharFirstPart = "!#$%&'*+-.0123456789/=?ABCDEFGHIJKLMNOPQRSTUVWXYZ^_`abcdefghijklmnopqrstuvwxyz{|}~";
-    if (first_part_email(str).length() < 1 || first_part_email(str).length() > 64) return false;
-
-    bool match = false;
-
-    for (int i = 0; i < first_part_email(str).length(); i++) {
-        match = false;
-        if (first_part_email(str)[0] == '.' || first_part_email(str)[first_part_email(str).length() - 1]
-            == '.' || first_part_email(str)[i] == '.' && first_part_email(str)[i]
-            == first_part_email(str)[i + 1] && first_part_email(str)[i] == '.') return false;
-        for (int j = 0; j < validCharFirstPart.length(); j++) {
-            if (first_part_email(str)[i] == validCharFirstPart[j]) {
-                match = true;
-                break;
-            }
+/*функция checkDotAndAt(string str) проверяет наличие 
+символов "@" и "." в строке. Она возвращает false, если 
+один из этих символов отсутствует в начале или конце строки. 
+Далее, эта функция проверяет, что символ "." не встречается 
+подряд, и возвращает false, если символ "@" встречается более 
+одного раза. Возвращаемое значение типа bool.*/
+bool checkDotAndAt(string str) {
+    if (str[0] == '.' || str[str.length() - 1] == '.') { return false; }
+    if (str[0] == '@' || str[str.length() - 1] == '@') { return false; }
+    bool statusDot = true;
+    bool statusAt = false;
+    for (unsigned int i = 1; i < str.length() - 1; i++) {
+        if (str[i] == '.') {
+            if (!statusDot) { return false; }
+            statusDot = false;
+        }   
+        else {
+            statusDot = true;
         }
-        if (!match) break;
-    }
-
-    if (match) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-
-bool second_part_is_valid(std::string str) {
-    if (!error_or_not(str)) return false;
-    std::string validCharSecondPart = "-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    std::string secondPartEmail;
-    for (int i = first_part_email(str).length() + 1; i < str.length(); i++) {
-        secondPartEmail += str[i];
-    }
-    if (secondPartEmail.length() < 1 || secondPartEmail.length() > 63) return false;
-
-    bool match = false;
-
-    for (int i = 0; i < secondPartEmail.length(); i++) {
-        if (secondPartEmail[0] == '.' || secondPartEmail[secondPartEmail.length() - 1] == '.' ||
-            secondPartEmail[i] == '.' && secondPartEmail[i] == secondPartEmail[i + 1] && secondPartEmail[i] == '.')
-            return false;
-        match = false;
-        for (int j = 0; j < validCharSecondPart.length(); j++) {
-            if (secondPartEmail[i] == validCharSecondPart[j]) {
-                match = true;
-                break;
-            }
+        if (str[i] == '@') {
+            if (statusAt) { return false; }
+            statusAt = true;
         }
-        if (!match) break;
     }
-
-    if (match) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return statusAt;
 }
 
-int main() {
-    std::string email;
-    std::cout << "Input the e-Mail address:" << '\n';
-    std::cin >> email;
-    std::cout << (first_part_is_valid(email) && second_part_is_valid(email) ? "Yes!" : "No!");
+/*функция checkChar(char ch) проверяет, является ли 
+переданный символ допустимым для email-адреса. Возвращает 
+true, если символ является буквой (a-z, A-Z), цифрой (0-9), 
+точкой "." или дефисом "-". Возвращаемое значение типа bool.*/
+bool checkChar(char ch) {
+    bool result = false;
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || ch == '.' || ch == '-') {
+        result = true;
+    }
+    return result;
+}
+
+/*функция checkSpecChars(char ch) проверяет, является ли 
+переданный символ специальным символом, которые могут 
+использоваться в email-адресах. Список специальных символов 
+определяется внутри функции. Если символ является одним из 
+специальных символов, то функция возвращает true, в противном 
+случае - false. Возвращаемое значение типа bool.*/
+bool checkSpecChars(char ch) {
+    string allowedChar = "!#$%&'*+-/=?^_`{|}~";
+    for (unsigned int i = 0; i < allowedChar.length(); i++) {
+        if (allowedChar[i] == ch) return true;
+    }
+    return false;
+}
+
+/*функция indexChar(string str, char ch) находит индекс
+первого вхождения символа ch в строке str. Если символ не 
+найден, то функция возвращает -1. Возвращаемое значение типа int.*/
+int indexChar(string str, char ch) {
+    int index = -1;
+    for (unsigned int i = 0; i < str.length(); i++) {
+        if (str[i] == ch) {
+            index = i;
+            return index;
+        }
+    }
+    return index;
+}
+
+/* функция getSubStr(string str, int begin, int end) 
+возвращает подстроку str, начинающуюся с индекса begin и 
+заканчивающуюся индексом end. Возвращаемое значение типа string.*/
+string getSubStr(string str, int begin, int end) {
+    string result;
+    for (int i = begin; i < end; i++) {
+        result += str[i];
+    }
+    return result;
+}
+
+/*функция beforeAt(string str) проверяет, что все символы 
+перед символом "@" допустимы для email-адреса. Эта функция 
+возвращает false, если длина перед символом "@" больше 64 
+или меньше 1, или если встречается символ, который не допустим 
+для email-адреса. Возвращаемое значение типа bool.*/
+bool beforeAt(string str) {
+    if (str.length() > 64 || str.length() < 1) return false;
+    for (int i = 0; i < str.length(); i++) {
+        if (!(checkChar(str[i]) || checkSpecChars(str[i]))) return false;
+    }
+    return true;
+}
+
+/*функция afterAt(string str) проверяет, что все символы 
+после символа "@" допустимы для email-адреса. Она возвращает 
+false, если длина после символа "@" больше 63 или меньше 1, 
+или если встречается символ, который не допустим для email-адреса. 
+Возвращаемое значение типа bool.*/
+bool afterAt(string str) {
+    if (str.length() > 63 || str.length() < 1) return false;
+    for (int i = 0; i < str.length(); i++) {
+        if (!checkChar(str[i])) return false;
+    }
+    return true;
+}
+
+/*функция isValidEmail(string str) проверяет, является ли 
+переданная строка действительным email-адресом. Она использует 
+предыдущие функции для проверки наличия символов "@" и ".", 
+корректности символов до и после "@", и возвращает true, если все провер*/
+bool isValidEmail(string str) {
+
+    bool result = false;
+    if (!checkDotAndAt(str)) return result;
+
+    if (beforeAt(getSubStr(str, 0, indexChar(str, '@')))
+        && afterAt(getSubStr(str, indexChar(str, '@') + 1, str.length()))) {
+        result = true;
+    }
+    return result;
+}
+
+int main()
+{
+    cout << "Enter your email: ";
+    string email;
+    cin >> email;
+    cin.ignore(32767, '\n');
+
+    cout << (isValidEmail(email) ? "Yes" : "No");
+
+    return 0;
 }
